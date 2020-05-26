63807748a1d600dc32f1fe274cb386199e6f0164
mi-ra0421 <65850234+mi-ra0421@users.noreply.github.com>
2020-05-26 22:21:51 +0900

    과제 제출

--- homework_week03_python_mira.py
+++ homework_week03_python_mira.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 # URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
 
 # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
 soup = BeautifulSoup(data.text, 'html.parser')
@@ -12,12 +12,17 @@ soup = BeautifulSoup(data.text, 'html.parser')
 songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
 #제목경로 : body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis 
+#rank 경로: body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
 
 for song in songs :
-    a_tag = soup.select_one('td.info > a.title.ellipsis')
+    a_tag = song.select_one('td.info > a.title.ellipsis')
     if a_tag is not None:
-        title = a_tag.text                                      
-        print(title)
+        rank =song.select_one('td.number').text  
+        title = a_tag.text  
+        artist = song.select_one('td.info > a.artist.ellipsis').text                     
+        print(rank.split()[0], title.strip(), artist)
+
+
 
 
  
