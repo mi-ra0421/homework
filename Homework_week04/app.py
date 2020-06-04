38d522651416badd191fa3971bceeaf837293b84
mi-ra0421 <65850234+mi-ra0421@users.noreply.github.com>
2020-06-04 19:14:49 +0900

    4주차 숙제 제출

--- Homework_week04/app.py
+++ Homework_week04/app.py
@@ -17,7 +17,7 @@ def home():
 def write_order():
     productnumber_receive = request.form['productnumber_give']
     color_receive = request.form['color_give']
-    name_receive = request.form['name_give']
+    buyername_receive = request.form['buyername_give']
     address_receive = request.form['address_give']
     phonenumber_receive = request.form['phonenumber_give']
 
@@ -25,11 +25,11 @@ def write_order():
     order = {
         'productnumber': productnumber_receive,
         'color': color_receive,
-        'name': name_receive,
+        'buyername': buyername_receive,
         'address': address_receive,
         'phonenumber': phonenumber_receive
     }
-    print(productnumber_receive)
+    print(order)
     # orders에 order 저장하기
 
     db.orders.insert_one(order)
@@ -41,9 +41,9 @@ def write_order():
 def read_orders():
 		# 1. DB에서 리뷰 정보 모두 가져오기
     orders = list(db.orders.find({},{'_id':0}))
-		# 2. 성공 여부 & 리뷰 목록 반환하기
+		# 2. 성공 여부 & 목록 반환하기
     return jsonify({'result': 'success', 'orders': orders})
-
+ 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
